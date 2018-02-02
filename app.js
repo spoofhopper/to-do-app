42106d8f2362c07665e15a8c8b1731146271b66d
Morgan Holland <morgan.holland@gmail.com>
2018-02-02 07:24:45 -0800

    refreshed app.js with delete functionality

--- app.js
+++ app.js
@@ -18,11 +18,26 @@ function onReady() {
     //  set the input's type to checkbox
     checkbox.type = 'checkbox';
 
+    //  create a delete button
+    let delete_button = document.createElement('input');
+
+    //  set the delete_button's type to button
+    delete_button.type = 'button';
+    delete_button.value = 'Delete';
+
+    // add delete functionality to delete_button
+    delete_button.addEventListener('click', event => {
+      toDoList.removeChild(newLi);
+    });
+
     //  set the title
     newLi.textContent = title;
 
     //  attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    //  attach the Delete button to the li
+    newLi.appendChild(delete_button);
 
     //  attach the li to the ul
     toDoList.appendChild(newLi);
